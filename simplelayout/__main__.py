bba3c52623b7d6670284ae7786c9f99e1b8e1c84
WJ Yao <996267113@qq.com>
2020-11-19 14:40:27 +0800

    完成

--- simplelayout/__main__.py
+++ simplelayout/__main__.py
@@ -1,9 +1,18 @@
-# TODO 正确导入函数 generate_matrix, save_matrix, save_fig
+from simplelayout.generator.utils import save_matrix, save_fig, make_dir
+from simplelayout.generator.core import generate_matrix
 from simplelayout.cli import get_options  # TODO: 保证不修改本行也可以正确导入
 
 
 def main():
-    raise NotImplementedError  # TODO 使用导入的函数按命令行参数生成数据，包括 mat 文件与 jpg 文件
+    options = get_options()
+    matrix = generate_matrix(options.board_grid,
+                             options.unit_grid,
+                             options.unit_n,
+                             options.positions)
+    make_dir(options.outdir)
+    path = "{}/{}".format(options.outdir, options.file_name)
+    save_matrix(matrix, path)
+    save_fig(matrix, path)
 
 
 if __name__ == "__main__":
